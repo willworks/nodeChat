272c6989a964762efbdff413af5d7cf8287ff1f6
willworks <scnuzhonghanjin@gmail.com>
2015-08-13 18:33:08 +0800

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(msg);
   });
 });
 
