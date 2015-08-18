d6b3be18425645a6dd8104e9184a4c96d22ce288
willworks <scnuzhonghanjin@gmail.com>
2015-08-18 21:18:20 +0800

    成功建立socket连接

--- client/js/client.js
+++ client/js/client.js
@@ -1,15 +1,28 @@
 window.onload = function(){
-	var socket = io();
-	var form = document.getElementsByTagName('form')[0];
-	consola.log(form);
-	var m = document.getElementById('m');
-	form.submit = function(){
-		socket.emit('chat message', m.value);
-		consola.log(m.value);
-		m.value = '';
-		return false;
-	}
-	// socket.on('chat message', function(msg){
-	// $('#messages').append($('<li>').text(msg));
-	// });
+	// 实例化nodeChat
+	var nodechat = new nodeChat();
+	nodechat.init();
 }
+
+// 定义nodeChat类别
+var nodeChat = function(){
+	this.socket = null;
+}
+
+
+//向原型添加业务方法
+nodeChat.prototype = {
+    init: function() {//此方法初始化程序
+        var that = this;
+        //建立到服务器的socket连接
+        this.socket = io.connect();
+        //监听socket的connect事件，此事件表示连接已经建立
+        this.socket.on('connect', function() {
+            //连接到服务器后，显示昵称输入框
+            document.getElementById('wait').textContent = "What's your name? :)";
+            document.getElementById('login').style.display = 'block';
+            document.getElementById('name').focus();
+            console.log('Connected successfully!');
+        });
+    }
+};
