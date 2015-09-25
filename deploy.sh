8e4143af2c99c253d302c417e81cd6f0b1658b8e
“willworks” <scnuzhonghanjin@gmail.com>
2015-09-25 13:18:17 +0800

    github webhook实现d代码自动更新

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Start autoDeployment"
+echo "pulling source code..."
+git pull
+echo "Finished."
--- server.js
+++ server.js
@@ -122,6 +122,26 @@ io.on('connection', function(socket){
 	});
 });
 
+// 运行shell脚本，pull代码
+function run_cmd(cmd, args, callback) {
+	var spawn = require('child_process').spawn;
+	var child = spawn(cmd, args);
+	var resp = "";
+	// 输出信息
+	child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+	child.stdout.on('end', function() { callback (resp);});
+}
+
+// 触发push事件的时候，可以发起一个POST请求
+// 处理github POST请求
+app.post('/', function(req, res){
+	if(req.headers["x-github-event"]=='push'){
+		run_cmd('sh', ['deploy.sh'], function(text){console.log(text);});
+	}
+	res.send('git pull success!');
+});
+
+
 http.listen(8080, function(){
 	console.log('listening on *:8080');
 });
