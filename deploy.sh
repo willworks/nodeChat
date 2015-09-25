5cb5709d116bd9c64309e1dc65e9575f5d878f70
“willworks” <scnuzhonghanjin@gmail.com>
2015-09-25 14:09:40 +0800

    自动部署shell脚本更新

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+$PATH='./'
+$USER='root'
+$USERGROUP='root'
+
 echo "Start autoDeployment"
+cd $PATH
 echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
 git pull
+git checkout master
+echo "changing permissions..."
+chown -R $USER:$USERGROUP $PATH
 echo "Finished."
